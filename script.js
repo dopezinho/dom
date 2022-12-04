9c6c52d1ec625700ab1eb762297f5b46f64b8db4
dopezinho <opugnero@gmail.com>
2022-12-04 14:48:18 -0300

    script

---
+++ script.js
@@ -0,0 +1,11 @@
+window.document.write('manipulano pela primera vez doido')
+//window.alert('salve doido (:')
+
+var x = window.document.getElementById('main')
+x.style.backgroundColor = '#fff'
+
+var y = window.document.getElementsByTagName('button')[0]
+y.innerHTML = 'sururu'
+
+var j = window.document.getElementsByTagName('button')[1]
+j.innerHTML = 'sondepin'
